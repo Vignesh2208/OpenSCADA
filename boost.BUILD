3173c004d2f6e3aa8ff7319cb0718d81e66e2c1b
Vignesh Babu <vig2208@gmail.com>
2019-01-31 18:23:46 -0600

    fixing bazel build problems and first successful run of test

--- boost.BUILD
+++
@@ -1,8 +0,0 @@
-cc_library(
-    name = "boost",
-    srcs = glob(["lib/*.so*"]),
-    hdrs = glob(["include/**/*.hpp", "include/**/*.h"]),
-    includes = ["include"],
-    visibility = ["//visibility:public"],
-    linkstatic = 1,
-)
--- src/pc_emulator/tests/test_utils.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <cstdlib>
-#include <assert.h>
-
-
-template <typename T> void EXPECT_EQ(string NestedFieldName,
-                                            int CategoryOfDataType);
